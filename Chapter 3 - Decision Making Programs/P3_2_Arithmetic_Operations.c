658e9e961a91c2bea08b3de716e4242331bbf2d4
Kaushal Tukadiya <kaushal.tukadiya1104@gmail.com>
2026-07-02 14:47:53 +0530

    Add files via upload

---
+++ Chapter 3 - Decision Making Programs/P3_2_Arithmetic_Operations.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the 1st Number A:");
+    scanf("%d",&a);
+    printf("Enter the 2nd Number B:");
+    scanf("%d",&b);
+    
+if(a>b)
+{
+    printf("A is Greater");
+}
+else
+{
+    printf("B is Greater");
+}
+return 0;
+}
