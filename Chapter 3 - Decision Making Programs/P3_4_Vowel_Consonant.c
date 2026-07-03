11841ff2eec726b8e3c7da203114a77ed511d9ed
Kaushal Tukadiya <kaushal.tukadiya1104@gmail.com>
2026-07-03 20:58:02 +0530

    Add files via upload

---
+++ Chapter 3 - Decision Making Programs/P3_4_Vowel_Consonant.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() 
+{
+    char ch;
+    printf("Enter a single alphabet character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+    {
+        
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+        {
+            printf("'%c' is a vowel.\n", ch);
+        } 
+        else if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') 
+        {
+            printf("'%c' is a vowel.\n", ch);
+        } 
+        else 
+        {
+            printf("'%c' is a consonant.\n", ch);
+        }
+    } 
+    
+    else
+    {
+        printf("Invalid input. Please enter an alphabet character.\n");
+    }
+
+    return 0;
+}
